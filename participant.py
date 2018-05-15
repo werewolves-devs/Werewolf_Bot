8e93533663a589c85758daa90b843079adebab97
Randium <33968886+Randium@users.noreply.github.com>
2018-05-15 21:53:10 +0200

    Minor update
    Just a few exceptions. Nothing much to write home for, but just... some additions that I made on my way home when my phone was out of battery.

--- participant.py
+++ participant.py
@@ -40,9 +40,31 @@ class Participant:
         # Make sure the pyromancer cannot kill themselves when powdered
         if self.role == "Pyromancer":
             self.powdered = False
-        
+        # Turn them if they're a cursed civilian and they caught attacked by wolves.
+        if self.role == "Cursed Civilian" and murderer in ["Werewolf", "Lone Wolf"]:
+            self.role = "Werewolf"
+            return "Werewolf"
+        # Turn them normal if they're a runner and they got attacked by wolves.
+        if self.role == "Runner" and murderer in ["Werewolf", "Lone Wolf"]:
+            self.role = "Innocent"
+            return "Innocent"
+        # If a wolf team member got attacked by a priest, ignore their status and kill them already.
+        if murderer == "Priest" and self.role in ["Werewolf", "Bloody Butcher", "Curse Caster", "Hell Hound", "Infected Wolf", "Lone Wolf", "Sacred Wolf", "Tanner", "Warlock", "White Werewolf", "Wolf's Cub"]:
+            self.role = "Dead"
+            return "Dead"
+        # If they die because a lover did, ignore their status and kill them already.
+        if murderer == "Cupid":
+            self.role = "Dead"
+            return "Dead"
+
+        # If none of these apply, just kill them.
         if murderer == "Pyromancer" and self.powdered == True:
             return self.death_phase(murderer)
+        if murderer == "Assassin":
+            return self.death_phase(murderer)
+        if murderer == "Cult Leader":
+            return self.death_phase(murderer)
+        
 
     # Kill the player, but check if they can be saved first. If not, kill them.
     def death_phase(self,murderer):
@@ -57,3 +79,6 @@ class Participant:
             self.undead = True
             self.demonized = False
             return "Undead"
+
+        self.role = "Dead"
+        return "Dead"
