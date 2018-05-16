491266f141bd9dc01d173634fa243b78a69c6b15
romangraef <roman.graef@gmail.com>
2018-05-16 16:38:23 +0200

    made participant not abstract anymore

--- lib/participant.py
+++ lib/participant.py
@@ -1,13 +1,11 @@
-from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .role import Role
 
 
-class Participant(metaclass=ABCMeta):
+class Participant(object):
     role: Role = None
 
-    @abstractmethod
     async def night(self):
         await self.role.night(self)
