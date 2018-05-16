0431d10304db8b5c519cd67c97c4048c6db63d98
Randium <33968886+Randium@users.noreply.github.com>
2018-05-16 15:15:41 +0200

    More details to the swap function!
    This way, no more list is needed nor is it necessary to register valid roles. This way, they can simply be added to the class function, defined in the class file, and done!

--- management/change_function.py
+++ management/change_function.py
@@ -2,14 +2,29 @@ import roles as role
 
 # This function changes a player's role to a given role.
 # Make sure the player doesn't already have the given role, for this function resets abilities like how many amulets the amulet holder has.
-def swap(rolename,name,id,channel,uses,votes,threatened,enchanted,demonized,powdered,frozen,undead,lovers,zombies,sleepers,souls,amulets):
+def swap(rolename,id,channel,uses,votes,threatened,enchanted,demonized,powdered,frozen,undead,bites,bitten,lovers,zombies,sleepers,souls,amulets):
+    # List of people that can kill most players
+    std_killers = ["Innocent", "Assassin", "Barber", "Cult Leader", "Executioner", "Huntress", "Witch", "Werewolf", "Lone Wolf", "Devil", "Wager", "Horseman", "Pyromancer"]
+    
     if rolename == "Innocent":
-        return role.Innocent(name,id,channel,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,lovers,zombies,sleepers,souls,amulets)
+        return role.Innocent("Innocent",id,channel,std_killers,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,bites,bitten,lovers,zombies,sleepers,souls,amulets)
     if rolename == "Alcoholic":
-        return role.Alcoholic(name,id,channel,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,lovers,zombies,sleepers,souls,amulets)
+        return role.Alcoholic("Alcoholic",id,channel,std_killers,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,bites,bitten,lovers,zombies,sleepers,souls,amulets)
     if rolename == "Amulet Holder":
-        return role.Amulet_Holder(name,id,channel,1,votes,threatened,enchanted,demonized,powdered,frozen,undead,lovers,zombies,sleepers,souls,amulets.append(id))
+        return role.Amulet_Holder("Amulet Holder",id,channel,std_killers,1,votes,threatened,enchanted,demonized,powdered,frozen,undead,bites,bitten,lovers,zombies,sleepers,souls,amulets.append(id))
     if rolename == "Assassin":
-        return role.Assassin(name,id,channel,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,lovers,zombies,sleepers,souls,amulets)
-    if rolename == "Aura Teller":
-        pass
+        return role.Assassin("Assassin",id,channel,std_killers,0,votes,threatened,enchanted,demonized,powdered,frozen,undead,bites,bitten,lovers,zombies,sleepers,souls,amulets)
+
+# Function that recognizes input of the user and translate it to what role it may refer to.
+# It returns the correct name of the role
+def role_interpret(user_msg):
+    # TODO
+    return "Spectator"
+    
+
+    
+# For testing purposes. This will not execute if this file is imported by another file.
+if __name__ == "__main__":
+    print role.Spectator("1","000000000")
+    rolerino = swap("Innocent","0","123456789",0,1,0,False,False,False,False,False,0,False,["Buddy1913","HurricanKai"],[],["FriedPotatoe"],-1,[])
+    print rolerino
