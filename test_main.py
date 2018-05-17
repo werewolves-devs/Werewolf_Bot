628b33fa63cfe7fa4f41ec2a6c2dcc7accf141ba
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-17 12:50:19 +0100

    Lol I can't code

--- test_main.py
+++ test_main.py
@@ -2,4 +2,4 @@
 # It will be better soon
 
 def test_universe_is_working_properly():
-  assert 1 = 1 #If this fails, something is seriously wrong
+  assert 1 == 1 #If this fails, something is seriously wrong
