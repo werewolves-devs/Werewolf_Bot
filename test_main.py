72c4950c5db27002e8f4294471fc59e6914b7abf
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-05-17 18:59:01 +0100

    Add tests for one function

--- test_main.py
+++ test_main.py
@@ -1,5 +1,14 @@
-#This file runs CI on Travis
+# This file runs CI on Travis
 # It will be better soon
 
+# Sanity checks
 def test_universe_is_working_properly():
   assert 1 == 1 #If this fails, something is seriously wrong
+
+
+# Functions module checks
+import functions
+def test_functions_module():
+    assert functions.check_for_int(5) == True
+    assert functions.check_for_int("absudHBASH") == False
+    assert functions.check_for_int("5") == True
