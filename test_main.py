27240afa7bdef46438df1c863fb2b13f0493fc87
Randium <33968886+Randium@users.noreply.github.com>
2018-06-08 15:27:51 +0200

    New testing command
    The thing can test if my commands work properly - do it!

--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
+from management.db import db_test
+
 #This file runs CI on Travis
 # It will be better soon
 
 def test_universe_is_working_properly():
   assert 1 == 1 #If this fails, something is seriously wrong
+
+  assert db_test() == 'Randium003'
