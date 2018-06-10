48958db9aa8a456146a736b624cd314dad6470c4
Randium <33968886+Randium@users.noreply.github.com>
2018-06-10 08:01:56 +0200

    Added positionof() tests
    The evil Dr. Travis will now inspect the positionof() command as well.

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from management.db import db_test
+from management.position import positionof
 
 #This file runs CI on Travis
 # It will be better soon
@@ -7,3 +8,10 @@ def test_universe_is_working_properly():
   assert 1 == 1 #If this fails, something is seriously wrong
 
   assert db_test() == 'Randium003'
+
+  assert positionof("frozen") == 13
+  assert positionof("fakerole") == 6
+  assert positionof("votes") == 8
+  assert positionof("sleepers") == 19
+  assert positionof("bitten") == 16
+  assert positionof("id") == 0
