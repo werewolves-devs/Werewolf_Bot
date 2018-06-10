08c4b82773f2f25052d49f3461a4c719e14a0862
Randium <33968886+Randium@users.noreply.github.com>
2018-06-10 15:12:29 +0200

    FIXING DEM PRINTRRRS
    Stupid parentheses...

--- management/db.py
+++ management/db.py
@@ -45,8 +45,7 @@ def db_get(user_id,column):
 
 # Change a user's bit of information in the database.
 def db_set(user_id,column,value):
-    # WARNING: Be VERY cautious with the column argument, as it is exploitable to SQL injection!
-    c.execute("UPDATE game SET {} = ? WHERE id= ?".format(column), (value,user_id))
+    c.execute("UPDATE game SET ?=? WHERE id=?", (column,value,user_id))
     conn.commit()
 
 # Add a new participant to the database
@@ -54,15 +53,8 @@ def signup(user_id,name,emoji):
     c.execute("INSERT INTO game (id,name,emoji,channel,role,fakerole,lovers,sleepers,amulets,zombies) VALUES (?,?,?,'#gamelog','Spectator','Spectator','','','','')", (user_id,name,emoji))
 
 def db_test():
-    print c.execute("SELECT * FROM game").fetchall()
-    print c.execute("INSERT INTO game (id,name,emoji,channel,role,fakerole,lovers,sleepers,amulets,zombies) VALUES ('1','Randium003',':smirk:','#gamelog','Spectator','Spectator','','','','')")
-    print get_user(1)
-    print db_get(1,'channel')
+    print(c.execute("INSERT INTO game (id,name,emoji,channel,role,fakerole,lovers,sleepers,amulets,zombies) VALUES ('1','Randium003',':smirk:','#gamelog','Spectator','Spectator','','','','')"))
+    print(get_user(1))
+    print(db_get(1,'channel'))
     # These changes aren't saved, making them safe to test.
-
-    print signup(2,'Buddy1913',':fire:')
-    print get_user(2)
-    print db_get(2,"role")
-    print db_set(2,"role","Assassin")
-    print get_user(2)
-    return db_get(2,'role')
+    return db_get(1,'name')
