73ddeabccc972739cc10b26d66c121ef79d8b40a
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-06-10 07:40:37 +0100

    Functions module doesn't exist anymore

--- test_main.py
+++ test_main.py
@@ -12,14 +12,6 @@ def test_universe_is_working_properly():
   assert 1 == 1 # If this fails, something is seriously wrong
   assert str(1) == "1" #Make sure python 6 hasn't released and changed all the names like minecraft did in 1.13 like seriously what was wrong with the old system there was nothing wrong with it why do I have to learn an entirely new system that I don't aaaaaaaaaaaa
 
-
-# Functions module checks
-import functions
-def test_functions_module():
-    assert functions.check_for_int(5) == True
-    assert functions.check_for_int("absudHBASH") == False
-    assert functions.check_for_int("5") == True
-
 # Check positionof() function
 def test_positionof():
   assert database_check() == 'Assassin'
