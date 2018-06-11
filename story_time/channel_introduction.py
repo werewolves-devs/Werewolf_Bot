0814217ddc9a54c3c673ba2a414a43c4b7849aab
Randium <33968886+Randium@users.noreply.github.com>
2018-06-11 22:43:54 +0200

    ed588's right.
    Let's do it like this, :b:oi

--- story_time/channel_introduction.py
+++ story_time/channel_introduction.py
@@ -1,36 +1,32 @@
 def channel_introduction(role):
-    '''
-    Do it like this:
-    '''
-    if role == "": # insert role here
-        msg = "Hi! This is your channel! " # The first one is without a plus
-        msg += "All the cool stuff happens in here! " # But all the others are with a plus
-        msg += "**I can still use ordinary Discord layout** _as you can see._ "
-        msg += "So yeah, that's pretty cool, huh?\n" # End lines with \n
-        msg += "I hope you enjoy. We do!"
-    '''
-    See? Not so difficult, huh?
-    '''
-    
-    '''
-    No, do it like THIS.
-    '''
+
+    # Do it like this:
     if role == "Assassin":
-        msg = """
+        # End lines with a \ if you do not actually want a new line to start
+        msg = """\
 **=========================**
 **WELCOME, ASSASSIN**
 **=========================**
-This town is like an old kitchen... it's a giant mess. Luckily, the town has *you*, 
+This town is like an old kitchen... it's a giant mess. Luckily, the town has *you*, \
 their protector, who picks out the cockroaches for them and squishes them like the puny animals they are. 
-You do not exactly know what is about to happen, but your senses never lie; 
+You do not exactly know what is about to happen, but your senses never lie; \
 something *TERRIBLE* is happening, and it depends on your actions how much the town will suffer from it!
-You do not know your allies, your masters taught you never to trust anyone. However,
+You do not know your allies, your masters taught you never to trust anyone. However, \
 you fear you can't do this alone. You must find the ones to save, and especially the ones to kill.
-**You are the assassin. Each night (except for the first one), you can attack and kill a player you suspect.
-Play the game whichever way you'd like, but try to keep the innocents alive. Good luck.**
+**You are the assassin. Each night (except for the first one), you can attack and kill a player you suspect.\
+Play the game whichever way you'd like, but try to keep the innocents alive. Good luck.**\
 """    
-    '''
-    see? much neater
-    '''
+    if role == "Aura Teller":
+        msg = """\
+**=========================**
+**WELCOME, AURA TELLER**
+**=========================**
+Not everyone in this town can be trusted - and you can feel it! You sense it in the people among you, \
+some have a... smell of evil around them. It is time to find out who is to be trusted, and who has solemnly \
+sworn to be up to no good!
+**You are the Aura Teller. Each night, you may inspect a player and find out if they are part of the wolf pack.\
+Remember that a positive aura does not necessarily mean they are on your team! Good luck.**\
+"""
+        
     # Ignore this. This allows us to see your message.
     return msg
