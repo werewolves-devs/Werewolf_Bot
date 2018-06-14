543e10e8dc74222c71f8ffb4dde55b18405979c6
romangraef <roman.graef@gmail.com>
2018-06-14 20:20:21 +0200

    basics for the poll command

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
-import discord
-import random
-import asyncio
+from discord import Embed, Color
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound, MissingPermissions, MissingRequiredArgument
 
+import config
 # Import config data
 from config import prefix
-import config
+from config import welcome_channel
+from utils import GameMastersOnly
 
-client = discord.Client()
+client = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
 
 '''
 To spare some time, the program should keep track of channels that it has sent messages in before.
 This way, sending messages to the right channels should (hopefully) save some time,
 as the bot doesn't need to ask the API for a channel for every message.
 Instead, it should save a table with the channel objects.
+
+
+no. they are cached anyway. it isnt a coroutine so it RUNS LOCALLY. PLEASE.
 '''
 channel_table = []
 
-# Whenever a message is sent.
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+
+async def on_command_error(ctx: commands.Context, exc: BaseException):
+    if isinstance(exc, CommandNotFound):
+        await ctx.send(exc.args[0])
+    if isinstance(exc, MissingPermissions):
+        await ctx.send(
+            embed=Embed(
+                description='You need to be an admin to execute this command - sorry',
+                color=Color.red()))
+    if isinstance(exc, MissingRequiredArgument):
+        await ctx.send(
+            embed=Embed(
+                color=Color.red(),
+                description='You are missing a required argument: ' + str(exc.param)))
+    if isinstance(exc, GameMastersOnly):
+        await ctx.send(
+            embed=Embed(
+                description='This command is only for GMs. Sorry.',
+                color=Color.red()))
+    else:
+        raise exc
+
+
+client.on_command_error = on_command_error
+
 
 # Whenever the bot regains his connection with the Discord API.
 @client.event
@@ -30,7 +54,10 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    
-    await client.send_message(client.get_channel(welcome_channel),'Beep boop! I just went online!')
+
+    await client.get_channel(welcome_channel).send('Beep boop! I just went online!')
+
+
+client.load_extension('polls')
 
 client.run(config.TOKEN)
