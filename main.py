a0ae523ab396a1ec3da257f1af214e84e3832602
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-06-16 09:09:14 +0100

    Clear things up

--- main.py
+++ main.py
@@ -12,25 +12,24 @@ initial_extensions = ['conspiracy_channels.main',
 from config import prefix
 import config
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
-bot = client # Either one can be referred to, though I believe we should be using bot
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
 
 # Whenever a message is sent.
-@client.event
+@bot.event
 async def on_message(message):
     # we do not want the bot to reply to itself
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
 # Whenever the bot regains his connection with the Discord API.
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-    await client.send_message(client.get_channel(config.welcome_channel),'Beep boop! I just went online!')
+    await bot.send_message(bot.get_channel(config.welcome_channel),'Beep boop! I just went online!')
     # Load extensions
     for extension in initial_extensions:
         bot.load_extension(extension)
@@ -40,4 +39,4 @@ async def test():
     print("Hi")
 
 
-client.run(config.TOKEN)
+bot.run(config.TOKEN)
