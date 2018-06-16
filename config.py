226d44ed2430d9a6220561a01bc75ac855fe7788
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-06-16 13:15:49 +0100

    Remove TODOs

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Discord API / Login settings
-TOKEN = 'put_token_here'
+TOKEN = 'NDAxNzE2MzgwODAzNTMwNzUy.DgWohA.lhaTqFX6aFR9a6usQxnlSRCi2mM'
 
 # Rules 'n' settings
 game_stage = 0
@@ -14,14 +14,14 @@ universal_prefix = "#!003-88-6521"
 database = 'database.db'
 
 # List of specific channels
-welcome_channel = 456880220486631424 #TODO: Channel where the bot says it's gone online.
+welcome_channel = 456880220486631424 # Channel where the bot says it's gone online.
 game_log = 1 #TODO
-bot_spam = 456880259636396034 #TODO
+bot_spam = 456880259636396034
 story_time = 3 #TODO
 
 # List of specific roles
-game_master = 456884637638328332 # TODO
-participant = 456881451745411089 #TODO
-dead_participant = 457498358077063170 #TODO
-frozen_participant = 457498631344357387 #TODO
+game_master = 456884637638328332
+participant = 456881451745411089
+dead_participant = 457498358077063170
+frozen_participant = 457498631344357387
 suspended = "55" #TODO
