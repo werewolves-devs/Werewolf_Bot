1de23f86f554bd80982118aeb2fd80cbcc7a9e16
Randium <33968886+Randium@users.noreply.github.com>
2018-06-17 21:53:04 +0200

    Fixed some Python 2.7 syntax errors
    'cause the files are still read, even if they're not executed.

--- interpretation/check.py
+++ interpretation/check.py
@@ -7,12 +7,12 @@ from management.position import roles_list
 # If the message contains emojis, they should be converted to ids as well. Mentions have priority, however.
 # The command should return the given amount of user ids, or, if equal to -1, should return them all.
 def users(message,amount = -1, delete_duplicates = True):
-    """Return the requested amount of user ids in a list. If the amount is -1, all users are given.  
-    
-    Keyword arguments:  
-    message -> the Discord message to inspect  
-    amount -> the wanted amount of users in a list  
-    delete_duplicates -> filter out users that are mentioned twice in the message  
+    """Return the requested amount of user ids in a list. If the amount is -1, all users are given.
+
+    Keyword arguments:
+    message -> the Discord message to inspect
+    amount -> the wanted amount of users in a list
+    delete_duplicates -> filter out users that are mentioned twice in the message
     """
     user_table = [person.id for person in message.mentions]
 
@@ -21,57 +21,57 @@ def users(message,amount = -1, delete_duplicates = True):
 
         if response != None:
             user_table.append(response)
-    
+
     if delete_duplicates == True:
         user_table = list(set(user_table))
-    
+
     if max(amount,1) > len(user_table):
         return False
 
     if amount == -1:
         return user_table
-    
+
     return [user_table[i] for i in range(amount)]
 
 
 # Makes sure the message has at least the needed amount of integers.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def numbers(message,amount = -1, delete_duplicates = False):
-    """Return the requested amount of integers in a list from the given message. If the amount is -1, all users are given.  
-    If there are not enough integers, or no integers at all, the function returns False.  
-    
-    Keyword arguments:  
-    message -> the Discord message to inspect  
-    amount -> the wanted amount of integers in the list  
-    delete_duplicates -> filter out integers that are mentioned twice in the message  
+    """Return the requested amount of integers in a list from the given message. If the amount is -1, all users are given.
+    If there are not enough integers, or no integers at all, the function returns False.
+
+    Keyword arguments:
+    message -> the Discord message to inspect
+    amount -> the wanted amount of integers in the list
+    delete_duplicates -> filter out integers that are mentioned twice in the message
     """
     number_table = []
 
     for argument in message.content.split(' '):
         if check_for_int(argument):
             number_table.append(int(argument))
-    
+
     if delete_duplicates == True:
         number_table = list(set(number_table))
-    
+
     if max(amount,1) > len(number_table):
         return False
-    
+
     if amount == -1:
         return number_table
-    
+
     return [number_table[i] for i in range(amount)]
 
 # Makes sure the message has at least the needed amount of emojis.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def emojis(message,amount = -1, delete_duplicates = True):
-    """Return the requested amount of (vanilla) emojis in a list from a message. If the amount is -1, all emojis are given.  
-    If there are not enough emojis or none at all, the function returns False.  
-    
-    Keyword arguments:  
-    message -> the Discord message to inspect  
-    amount -> the wanted amount of integers in the list  
-    delete_duplicates -> filter out emojis that are mentioned twice in the message  
+    """Return the requested amount of (vanilla) emojis in a list from a message. If the amount is -1, all emojis are given.
+    If there are not enough emojis or none at all, the function returns False.
+
+    Keyword arguments:
+    message -> the Discord message to inspect
+    amount -> the wanted amount of integers in the list
+    delete_duplicates -> filter out emojis that are mentioned twice in the message
     """
 
     emoji_table = []
@@ -79,28 +79,28 @@ def emojis(message,amount = -1, delete_duplicates = True):
     for argument in message.content.split(' '):
         if argument in UNICODE_EMOJI:
             emoji_table.append(argument)
-    
+
     if delete_duplicates == True:
         emoji_table = list(set(emoji_table))
-    
+
     if max(amount,1) > len(emoji_table):
         return False
-    
+
     if amount == -1:
         return emoji_table
-    
+
     return [emoji_table[i] for i in range(amount)]
 
 # Makes sure the message has at least the needed amount of roles.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def roles(message,amount= -1,delete_duplicates = False):
-    """Return the requested amount of roles that are mentioned in the message as a list. If the amount is -1, all roles are given.  
-    If there are not enough roles or none at all, the function returns False.  
-    
-    Keyword arguments:  
-    message -> the Discord message to inspect  
-    amount -> the wanted amount of integers in the list  
-    delete_duplicates -> filter out roles that are mentioned twice in the message  
+    """Return the requested amount of roles that are mentioned in the message as a list. If the amount is -1, all roles are given.
+    If there are not enough roles or none at all, the function returns False.
+
+    Keyword arguments:
+    message -> the Discord message to inspect
+    amount -> the wanted amount of integers in the list
+    delete_duplicates -> filter out roles that are mentioned twice in the message
     """
 
     role_table = []
@@ -108,16 +108,16 @@ def roles(message,amount= -1,delete_duplicates = False):
     for argument in message.content.split(' '):
         if argument in roles_list:
             role_table.append(argument)
-    
+
     if delete_duplicates == True:
         role_table = list(set(role_table))
-    
+
     if max(amount,1) > len(role_table):
         return False
-    
+
     if amount == -1:
         return role_table
-    
+
     return [role_table[i] for i in range(amount)]
 
 # Checks if a file can be converted into an integer.
@@ -132,12 +132,12 @@ def check_for_int(s):
 
 # Checks if an input requests a given command
 def is_command(message,commandlist,help=False):
-    """Check if the message starts with the given command or its aliases.  
-    
-    Keyword arguments:  
-    message -> the Discord message  
-    commandlist -> list of possible commands that return True if the prefix is put in front of them  
-    help -> when set to True, return True when the message starts with the prefix, then help, and then the command.  
+    """Check if the message starts with the given command or its aliases.
+
+    Keyword arguments:
+    message -> the Discord message
+    commandlist -> list of possible commands that return True if the prefix is put in front of them
+    help -> when set to True, return True when the message starts with the prefix, then help, and then the command.
     """
     for command in commandlist:
         if message.content.startswith(prefix + command) and help == False:
@@ -149,8 +149,8 @@ def is_command(message,commandlist,help=False):
 if __name__ == "__main__":
     class message:
         content = "Deze tekst is Nederlands, maar bevat 4 cijfers; 1999 8 en 1 ! Jazeker, dat zijn er vier."
-    
+
     x = message()
-    print numbers(x)
-    print numbers(x,3)
-    print numbers(x,5)
+    print(numbers(x))
+    print(numbers(x,3))
+    print(numbers(x,5))
