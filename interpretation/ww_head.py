c9c2aade40cf6ce065e330c2e3c4997f619619ec
Randium <33968886+Randium@users.noreply.github.com>
2018-06-17 21:50:54 +0200

    Added alternative message
    At least send out a friendly "hi!" when nothing's being seen.

--- interpretation/ww_head.py
+++ interpretation/ww_head.py
@@ -1,10 +1,10 @@
 # This is the main file that cuts the message into pieces and transfers the info the the map roles_n_rules.
 from management.db import isParticipant, personal_channel, db_get, db_set
-from check import is_command
+from interpretation.check import is_command
 import roles_n_rules.functions as func
-from main_classes import Mailbox
+from main_classes import Mailbox, Message
 from config import prefix
-import check
+import interpretation.check
 
 def todo():
     return [Mailbox().spam("I am terribly sorry! This command doesn't exist yet!",True)]
@@ -12,7 +12,7 @@ def todo():
 def process(message, isGameMaster = False):
 
     user_id = message.author.id
-    message_channel = message.channel
+    message_channel = message.channel.id
     user_role = db_get(user_id,'role')
 
     # =============================================================
@@ -46,9 +46,9 @@ def process(message, isGameMaster = False):
 
         if is_command(message,['assign'],True):
             msg = "**Usage:** `" + prefix + "assign <user> <role>`\n\nExample: `" + prefix
-            msg += "assign @Randium#6521 Innocent`\nGame Master only command" 
+            msg += "assign @Randium#6521 Innocent`\nGame Master only command"
             return [Mailbox().spam(msg)]
-        
+
         '''day'''
         # This command is used to initialize the day.
         if is_command(message,['day']):
@@ -80,12 +80,12 @@ def process(message, isGameMaster = False):
                 role = db_get(user,'role')
                 msg = "{} - <@{}> has the role of the `{}`!".format(emoji,user,role)
                 identities.spam(msg)
-            
+
             return [identities]
 
         if is_command(message,['whois'],True):
             msg = "**Usage:** `" + prefix + "whois <user1> <user2> ...`\n\n"
-            msg += "Example: `" + prefix + "whois @Randium#6521`\nGame Master only command" 
+            msg += "Example: `" + prefix + "whois @Randium#6521`\nGame Master only command"
 
     # =============================================================
     #
@@ -132,7 +132,7 @@ def process(message, isGameMaster = False):
         if is_command(message,['myrole'],True):
             # TODO
             return todo()
-        
+
         '''remove'''
         # This command removes a given user from a conspiracy channel.
         # A user should not get removed if they're the channel owner.
@@ -167,7 +167,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['assassinate','kill'],True) and user_role == "Assassin":
                 # TODO
                 return todo()
-            
+
             '''aura'''
             # The command for aura tellers
             if is_command(message,['aura','tell','vision']) and user_role == "Aura Teller":
@@ -176,7 +176,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['aura','tell','vision'],True) and user_role == "Aura Teller":
                 # TODO
                 return todo()
-            
+
             '''barber_kill'''
             # Barber kill - to assassinate a victim during the day
             if is_command(message,['assassinate','barber_kill','cut']) and user_role == "Barber":
@@ -185,7 +185,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['assassinate','barber_kill','cut'],True) and user_role == "Barber":
                 # TODO
                 return todo()
-            
+
             '''seek'''
             # Crowd seeker's power
             if is_command(message,['crowd','seek']) and user_role == "Crowd Seeker":
@@ -194,7 +194,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['crowd','seek'],True) and user_role == "Crowd Seeker":
                 # TODO
                 return todo()
-            
+
             '''kiss'''
             # Cupid's power to fall in love with someone.
             if is_command(message,['kiss','love','shoot']) and user_role == "Cupid":
@@ -212,7 +212,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['bark','become','choose','follow'],True) and user_role == "Dog":
                 # TODO
                 return todo()
-            
+
             '''execute'''
             # This command allows the executioner to choose a replacement target.
             if is_command(message,['choose','execute']) and user_role == "Executioner":
@@ -230,7 +230,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['exercise','exorcise','undoom'],True) and user_role == "Exorcist":
                 # TODO
                 return todo()
-            
+
             '''inspect'''
             # The fortune teller's command.
             if is_command(message,['forsee','inspect','see','tell']) and user_role == "Fortune Teller":
@@ -248,7 +248,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['knit','knot','silence'],True) and user_role == "Grandma":
                 # TODO
                 return todo()
-            
+
             '''hook'''
             # The hooker's command
             if is_command(message,['fuck','hook','sleep']) and user_role == "Hooker":
@@ -257,7 +257,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['fuck','hook','sleep'],True) and user_role == "Hooker":
                 # TODO
                 return todo()
-            
+
             '''hunt'''
             # The huntress' command. Used to keep track of whom will be shot.
             if is_command(message,['hunt','shoot']) and user_role == "Huntress":
@@ -266,7 +266,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['hunt','shoot'],True) and user_role == "Huntress":
                 # TODO
                 return todo()
-            
+
             '''unfreeze'''
             # The innkeeper's command
             if is_command(message,['melt','unfreeze']) and user_role == "Innkeeper":
@@ -275,7 +275,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['melt','unfreeze'],True) and user_role == "Innkeeper":
                 # TODO
                 return todo()
-            
+
             '''copy'''
             # The Look-Alike's command
             if is_command(message,['copy','imitate','mirror','resemble']) and user_role == "Look-Alike":
@@ -284,7 +284,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['copy','imitate','mirror','resemble'],True) and user_role == "Look-Alike":
                 # TODO
                 return todo()
-            
+
             '''holify'''
             # The Priest's command
             if is_command(message,['holify','sacrify','water']) and user_role == "Priest":
@@ -293,7 +293,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['holify','sacrify','water'],True) and user_role == "Priest":
                 # TODO
                 return todo()
-            
+
             '''purify'''
             # The Priestess' command
             if is_command(message,['heal','light','purify','sacrify']) and user_role == "Priestess":
@@ -311,7 +311,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['threaten','raven'],True) and user_role == "Raven":
                 # TODO
                 return todo()
-            
+
             '''reveal'''
             # The Royal Knight's command
             if is_command(message,['end','prevent','reveal','stop']) and user_role == "Royal Knight":
@@ -329,7 +329,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['heal','life','save'],True) and user_role == "Witch":
                 # TODO
                 return todo()
-            
+
             '''death'''
             # The witch' command to use her death potion
             if is_command(message,['death','kill','murder','poison']) and user_role == "Witch":
@@ -338,7 +338,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['death','kill','murder','poison'],True) and user_role == "Witch":
                 # TODO
                 return todo()
-            
+
             '''çurse'''
             # The curse caster's command
             if is_command(message,['cast','corrupt','curse']) and user_role == "Curse Caster":
@@ -356,7 +356,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['cough','infect','sneeze','turn'],True) and user_role == "Infected Wolf":
                 # TODO
                 return todo()
-            
+
             '''devour'''
             # The Lone wolf's command
             if is_command(message,['chew','devour','eat','kill','munch']) and user_role == "Lone Wolf":
@@ -365,7 +365,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['chew','devour','eat','kill','munch'],True) and user_role == "Lone Wolf":
                 # TODO
                 return todo()
-            
+
             '''disguise'''
             # The tanner's command
             if is_command(message,['change','cloth','disguise','hide']) and user_role == "Tanner":
@@ -374,7 +374,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['change','cloth','disguise','hide'],True) and user_role == "Tanner":
                 # TODO
                 return todo()
-            
+
             '''inspect'''
             # The Warlock's command
             if is_command(message,['forsee','inspect','see','tell']) and user_role == "Priestess":
@@ -383,7 +383,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['forsee','inspect','see','tell'],True) and user_role == "Priestess":
                 # TODO
                 return todo()
-            
+
             '''devour'''
             # The white werewolf's command
             if is_command(message,['chew','devour','eat','kill','munch']) and user_role == "White Werewolf":
@@ -392,7 +392,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['chew','devour','eat','kill','munch'],True) and user_role == "White Werewolf":
                 # TODO
                 return todo()
-            
+
             '''wager'''
             # The devil's command
             if is_command(message,['choose','wager']) and user_role == "Devil":
@@ -401,7 +401,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['choose','wager'],True) and user_role == "Devil":
                 # TODO
                 return todo()
-            
+
             '''enchant'''
             # The flute player's command
             if is_command(message,['enchant','flute']) and user_role == "Flute Player":
@@ -410,7 +410,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['enchant','flute'],True) and user_role == "Flute Player":
                 # TODO
                 return todo()
-            
+
             '''unite'''
             # The horseman's command
             if is_command(message,['apocalypse','clean','unite']) and user_role == "Horseman":
@@ -419,7 +419,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['apocalypse','clean','unite'],True) and user_role == "Horseman":
                 # TODO
                 return todo()
-            
+
             '''guess'''
             # The ice king's command to add a guess about a user to their list.
             # Note that this command could/should be usable at any time, as long as the submit command isn't
@@ -429,7 +429,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['add','guess','freeze'],True) and user_role == "Ice King":
                 # TODO
                 return todo()
-            
+
             '''submit'''
             # The ice king's command to submit the list of people of whom they have guessed their roles.
             if is_command(message,['guess_that','freeze_all','submit']) and user_role == "Ice King":
@@ -438,7 +438,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['guess_that','freeze_all','submit'],True) and user_role == "Ice King":
                 # TODO
                 return todo()
-            
+
             '''powder'''
             # Powder a player
             if is_command(message,['creeper','powder']) and user_role == "Pyromancer":
@@ -447,7 +447,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['creeper','powder'],True) and user_role == "Pyromancer":
                 # TODO
                 return todo()
-            
+
             '''abduct'''
             # To kidnap players
             if is_command(message,['abduct','add','kidnap','swamp']) and user_role == "The Thing":
@@ -456,7 +456,7 @@ def process(message, isGameMaster = False):
             if is_command(message,['abduct','add','kidnap','swamp'],True) and user_role == "The Thing":
                 # TODO
                 return todo()
-            
+
             '''create_swamp'''
             # To create a new swamp with all victims
             if is_command(message,['abduct_all','create_swamp','start_cliche_horror_movie']) and user_role == "The Thing":
@@ -499,3 +499,5 @@ def process(message, isGameMaster = False):
     if is_command(message,['signup'],True):
         # TODO
         return todo()
+
+    return [Mailbox().msg("Sorry bud, couldn't find what you were looking for.",message_channel,True).msg("But hey, at least SOME response!",message_channel,True)]
