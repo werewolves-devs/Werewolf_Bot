bdc016b0f12df0d37205d1937e6c5730b8eedeee
Randium <33968886+Randium@users.noreply.github.com>
2018-06-17 19:09:35 +0200

    Addede automatic explanation for commands
    Yay! We should now receive some explanations for our code!

--- interpretation/check.py
+++ interpretation/check.py
@@ -7,6 +7,13 @@ from management.position import roles_list
 # If the message contains emojis, they should be converted to ids as well. Mentions have priority, however.
 # The command should return the given amount of user ids, or, if equal to -1, should return them all.
 def users(message,amount = -1, delete_duplicates = True):
+    """Return the requested amount of user ids in a list. If the amount is -1, all users are given.  
+    
+    Keyword arguments:  
+    message -> the Discord message to inspect  
+    amount -> the wanted amount of users in a list  
+    delete_duplicates -> filter out users that are mentioned twice in the message  
+    """
     user_table = [person.id for person in message.mentions]
 
     for argument in message.content.split(' '):
@@ -30,6 +37,14 @@ def users(message,amount = -1, delete_duplicates = True):
 # Makes sure the message has at least the needed amount of integers.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def numbers(message,amount = -1, delete_duplicates = False):
+    """Return the requested amount of integers in a list from the given message. If the amount is -1, all users are given.  
+    If there are not enough integers, or no integers at all, the function returns False.  
+    
+    Keyword arguments:  
+    message -> the Discord message to inspect  
+    amount -> the wanted amount of integers in the list  
+    delete_duplicates -> filter out integers that are mentioned twice in the message  
+    """
     number_table = []
 
     for argument in message.content.split(' '):
@@ -50,6 +65,15 @@ def numbers(message,amount = -1, delete_duplicates = False):
 # Makes sure the message has at least the needed amount of emojis.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def emojis(message,amount = -1, delete_duplicates = True):
+    """Return the requested amount of (vanilla) emojis in a list from a message. If the amount is -1, all emojis are given.  
+    If there are not enough emojis or none at all, the function returns False.  
+    
+    Keyword arguments:  
+    message -> the Discord message to inspect  
+    amount -> the wanted amount of integers in the list  
+    delete_duplicates -> filter out emojis that are mentioned twice in the message  
+    """
+
     emoji_table = []
 
     for argument in message.content.split(' '):
@@ -70,6 +94,15 @@ def emojis(message,amount = -1, delete_duplicates = True):
 # Makes sure the message has at least the needed amount of roles.
 # The command should return the given amount of numbers, or, if equal to -1, should return them all.
 def roles(message,amount= -1,delete_duplicates = False):
+    """Return the requested amount of roles that are mentioned in the message as a list. If the amount is -1, all roles are given.  
+    If there are not enough roles or none at all, the function returns False.  
+    
+    Keyword arguments:  
+    message -> the Discord message to inspect  
+    amount -> the wanted amount of integers in the list  
+    delete_duplicates -> filter out roles that are mentioned twice in the message  
+    """
+
     role_table = []
 
     for argument in message.content.split(' '):
@@ -90,6 +123,7 @@ def roles(message,amount= -1,delete_duplicates = False):
 # Checks if a file can be converted into an integer.
 # If it cannot, the function returns false.
 def check_for_int(s):
+    """Returns True if the value s is or can be converted to an integer. Returns False otherwise."""
     try:
         int(s)
         return True
@@ -98,6 +132,13 @@ def check_for_int(s):
 
 # Checks if an input requests a given command
 def is_command(message,commandlist,help=False):
+    """Check if the message starts with the given command or its aliases.  
+    
+    Keyword arguments:  
+    message -> the Discord message  
+    commandlist -> list of possible commands that return True if the prefix is put in front of them  
+    help -> when set to True, return True when the message starts with the prefix, then help, and then the command.  
+    """
     for command in commandlist:
         if message.content.startswith(prefix + command) and help == False:
             return True
