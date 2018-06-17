a46e6f50a0d9425fdf19c6218e97ff4d267d6f15
Randium <33968886+Randium@users.noreply.github.com>
2018-06-17 21:55:41 +0200

    Handle None-exception
    Right. Didn't think of that, my bad.
    Errare humanum est.

--- management/db.py
+++ management/db.py
@@ -7,11 +7,11 @@ c = conn.cursor()
 
 def execute(cmd_string):
     """Execute a command straight into the database. Avoiding usage recommended.
-    
-    Keyword arguments:  
-    cmd_string -> the command to be executed upon the database  
+
+    Keyword arguments:
+    cmd_string -> the command to be executed upon the database
     """
-    
+
     c.execute(cmd_string)
 
     conn.commit()
@@ -19,7 +19,7 @@ def execute(cmd_string):
     return c.fetchall()
 
 def poll_list():
-    """Return a list of users to be added to the poll.  
+    """Return a list of users to be added to the poll.
     The first argument is the user's id, the second is their emoji, and the third and fourth are arguments that no longer\
     allow the user to vote on them."""
 
@@ -32,9 +32,9 @@ def poll_list():
 def emoji_to_player(emoji):
     """Look up the user's id that corresponds with the given emoji. Returns None if the user doesn't exist, \
     and returns the user's id if present.
-    
-    Keyword arguments:  
-    emoji -> given emoji  
+
+    Keyword arguments:
+    emoji -> given emoji
     """
 
     c.execute("SELECT id FROM game WHERE emoji =?", (emoji,))
@@ -51,9 +51,9 @@ def emoji_to_player(emoji):
 # Get all of a user's data from the database
 def get_user(id):
     """Gather all of a user's data from the database.
-    
-    Keyword arguments:  
-    id -> the user's id  
+
+    Keyword arguments:
+    id -> the user's id
     """
 
     c.execute("SELECT * FROM game WHERE id=?", (id,))
@@ -71,19 +71,19 @@ def get_user(id):
 # If the user is a spectator, it returns whatever spectator is set to.
 def isParticipant(id,spectator = False,dead = False):
     """Checks if the user is a registered participant in the database
-    
-    Keyword arguments:  
-    id -> the user's id  
-    spectator -> value the function should return if the user is a spectator  
-    dead -> value the function should return if the user is dead  
+
+    Keyword arguments:
+    id -> the user's id
+    spectator -> value the function should return if the user is a spectator
+    dead -> value the function should return if the user is dead
     """
 
     if get_user(id) in [None, []]:
         return False
-    
+
     if db_get(id,"role") == u'Spectator':
         return spectator
-    
+
     if db_get(id,'role') == u'Dead':
         return dead
 
@@ -92,30 +92,33 @@ def isParticipant(id,spectator = False,dead = False):
 # This function returns a user's personal channel.
 def personal_channel(user_id,channel_id):
     """Returns True if the given channel is the user's personal channel.
-    
-    Keyword arguments:  
-    user_id -> the user's id  
-    channel_id -> the channel's id  
+
+    Keyword arguments:
+    user_id -> the user's id
+    channel_id -> the channel's id
     """
     if db_get(user_id,"channel") == str(channel_id):
         return True
-    
+
     return False
 
 # Gather a user's bit of information from the database.
 def db_get(user_id,column):
     """Gain a specific bit of information from a given player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
-    column -> the relevant part of info  
+
+    Keyword arguments:
+    user_id -> the user's id
+    column -> the relevant part of info
     """
-    return get_user(user_id)[positionof(column)]
+    values = get_user(user_id)
+    if values == None:
+        return None
+    return values[positionof(column)]
 
 # Change a user's bit of information in the database.
 def db_set(user_id,column,value):
     """Alter a specific bit of information of a given player
-    
+
     Keyword argumentsL
     user_id -> the user's id
     column -> the relevant part of info
@@ -128,14 +131,14 @@ def db_set(user_id,column,value):
 # Apply in case of an end-effect kill.
 def add_kill(victim_id,role,murderer = ""):
     """Add a new order to the kill queue.
-    
-    Keyword arguments:  
-    victim_id -> the id of the victim to be attacked  
-    role -> the role of the attacker  
-    murderer -> id of the attacker (random attacker if multiple)  
+
+    Keyword arguments:
+    victim_id -> the id of the victim to be attacked
+    role -> the role of the attacker
+    murderer -> id of the attacker (random attacker if multiple)
     """
     data = [victim_id,role,murderer]
-    c.execute("INSERT INTO 'death-row' ('id','victim','role','murderer') VALUES (NULL,?,?,?)",data)    
+    c.execute("INSERT INTO 'death-row' ('id','victim','role','murderer') VALUES (NULL,?,?,?)",data)
     conn.commit()
     return
 
@@ -146,7 +149,7 @@ def get_kill():
 
     try:
         order = c.fetchone()
-        
+
         if order == None:
             return None
     except TypeError:
@@ -162,27 +165,27 @@ def get_kill():
 # Register a new channel to the database
 def add_channel(channel_id,owner):
     """Add a channel to the database.
-    
-    Keyword arguments:  
-    channel_id -> the channel's id  
-    owner -> the owner's id  
+
+    Keyword arguments:
+    channel_id -> the channel's id
+    owner -> the owner's id
     """
     c.execute("INSERT INTO 'channels' ('channel_id','owner') VALUES (?,?)",(channel_id,owner))
     conn.commit()
 
 # Change a user's value in a specific channel
 def set_user_in_channel(channel_id,user_id,number):
-    """Set a specific user's value in a given channel.  
-    # 0 - no access  
-    # 1 - access  
-    # 2 - frozen  
-    # 3 - abducted  
-    # 4 - dead  
+    """Set a specific user's value in a given channel.
+    # 0 - no access
+    # 1 - access
+    # 2 - frozen
+    # 3 - abducted
+    # 4 - dead
 
-    Keyword arguments:  
-    channel_id -> the channel's id  
-    user_id -> the user's id  
-    number -> the value to set  
+    Keyword arguments:
+    channel_id -> the channel's id
+    user_id -> the user's id
+    number -> the value to set
     """
     data = [number,channel_id]
     c.execute("UPDATE \"channels\" SET \"id{}\"=? WHERE \"channel_id\" =?".format(user_id),data)
@@ -192,11 +195,11 @@ def set_user_in_channel(channel_id,user_id,number):
 # It then returns all channels that it has changed.
 def channel_change_all(user_id,old,new):
     """Change all values from one to another for a given user. Returns a list of altered channels.
-    
-    Keyword arguments:  
-    user_id -> the user's id  
-    old -> the old value to change  
-    new -> the new value to change to  
+
+    Keyword arguments:
+    user_id -> the user's id
+    old -> the old value to change
+    new -> the new value to change to
     """
     c.execute("SELECT channel_id FROM 'channels' WHERE id{} =?".format(user_id),(old,))
     change_list = c.fetchall()
@@ -213,8 +216,8 @@ def channel_change_all(user_id,old,new):
 def channel_get(channel_id,user_id = ''):
     """Gain the information of a channel.
 
-    Keyword arguments:  
-    channel_id -> the channel's id  
+    Keyword arguments:
+    channel_id -> the channel's id
     user_id -> (optional) the specific value. Returns all info if blank and returns the owner's id when set to 'owner'
     """
     if user_id == '':
@@ -239,41 +242,41 @@ def get_columns():
 
 def abduct(user_id):
     """Returns a list of channels that need to be changed to abduct a player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
+
+    Keyword arguments:
+    user_id -> the user's id
     """
     return channel_change_all(user_id,1,3)
 
 def unabduct(user_id):
     """Returns a list of channels that need to be changed to unabduct a player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
+
+    Keyword arguments:
+    user_id -> the user's id
     """
     return channel_change_all(user_id,3,1)
 
 def freeze(user_id):
     """Returns a list of channels that need to be changed to freeze a player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
+
+    Keyword arguments:
+    user_id -> the user's id
     """
     return channel_change_all(user_id,1,2)
 
 def unfreeze(user_id):
     """Returns a list of channels that need to be changed to unfreeze a player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
+
+    Keyword arguments:
+    user_id -> the user's id
     """
     return channel_change_all(user_id,2,1)
 
 def kill(user_id):
     """Returns a list of channels that need to be changed to kill a player
-    
-    Keyword arguments:  
-    user_id -> the user's id  
+
+    Keyword arguments:
+    user_id -> the user's id
     """
     return [channel_change_all(user_id,i,4) for i in range(4)]
 
