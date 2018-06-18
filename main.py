baaf34fcb4573f3b8f0959263db4eb76f8315de3
Randium <33968886+Randium@users.noreply.github.com>
2018-06-18 22:59:03 +0200

    Add channel creation to main.py
    Work still needs to be done. But this is what we've got for now.

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ async def on_message(message):
         for element in mailbox.newchannels:
             # element.name - name of the channel;
             # element.owner - owner of the channel;
+            # element.members - members to be added to the channel [NEEDS TO BE MADE YET]
             #
             # @Participant      - no view + type
             # @dead Participant - view + no type
@@ -86,6 +87,61 @@ async def on_message(message):
 
             # TODO
 
+            if not message_author in element.members:
+                element.members.append(message_author)
+
+            message = creation_messages.cc_intro(member_ids)
+            
+            # Role objects (based on ID)
+            main_guild = client.get_channel(bot_spam).guild # Find the guild we're in
+            roles = main_guild.roles # Roles from the guild
+            game_master_role = discord.utils.find(lambda r: r.id == game_master, roles) #WUSDIS                     # \
+            dead_participant_role = discord.utils.find(lambda r: r.id == dead_participant, roles) #WUSDIS           # | Find various role objects
+            frozen_participant_role = discord.utils.find(lambda r: r.id == frozen_participant, roles) #WUSDIS       # /
+            default_permissions = {
+                main_guild.default_role: discord.PermissionOverwrite(read_messages=False),                     # \
+                frozen_participant_role: discord.PermissionOverwrite(send_messages=False),                     # |
+                dead_participant_role: discord.PermissionOverwrite(read_messages=True, send_messages=False),   # | Set the permissions for each role and the bot user
+                game_master_role: discord.PermissionOverwrite(read_messages=True),                             # |
+                self.bot.user: discord.PermissionOverwrite(read_messages=True),                                # /
+                **{                                                                               # \
+                    member: discord.PermissionOverwrite(read_messages=True) for member in element.members # | Set permissions for each member
+                },                                                                                # /
+            }
+
+            with open("conspiracy_channels/cc_data.json") as cc_data:
+                try:
+                    data = json.load(cc_data) # Try to load conspiracy channel data from a file, ./cc_data.json
+                except JSONDecodeError:
+                    return await client.get_channel(message.channel).send('cc_data was not found or is invalid.') # If we can't find it or it's invalid, throw an error
+            try:
+                category = client.get_channel(data['category_id']) # Find the category based on the data from cc_data
+            except:
+                #Category couldn't be found, let's make a new one
+                category = await main_guild.create_category('Conspiracy Channels', reason='Old CC Category not found; Creating new one') # TODO: Include season code etc
+            if len(category.channels) > 49:
+                # Current category is full, make a new one!
+                category = await main_guild.create_category('Conspiracy Channels', reason='Old CC Category full; Creating new one') # TODO: Include season code etc
+            try:
+                try:
+                    channel = await ctx.guild.create_text_channel( # Create a text channel
+                        name, # With the name 'name'
+                        category=category, # In the category 'category' (A variable we just defined earlier)
+                        overwrites=default_permissions, # Set the permissions to the 'default_permissions' object we created earlier
+                        reason='Conspiracy Channel creation requested by ' + ctx.author.mention) # Set the reason for the Audit Log
+                    await channel.send(message) # Send our welcome message from cc_intro
+                except Exception as e: # Catch any thrown exceptions and send an error to the user
+                    await ctx.channel.send('There was an error creating the channel, likely role finding. Please contact a Game Master for more info.\n\n*Game Masters: Check the console*')
+                    raise e # Send the full info to console
+            except Exception as e:
+                # Something went wrong. Let's tell the user.
+                ctx.message.channel.send("Something went wrong. Please contact a Game Master for additional assistance.\n\n*Game Masters: Check the console*")
+                raise e
+
+            data['category_id'] = category.id
+            with open("conspiracy_channels/cc_data.json", 'w') as cc_data:
+                json.dump(data, cc_data) # Rewrite our 'data' back to disk, as it may have been modified
+            
             for buddy in element.settlers:
                 db_set(buddy,"channel",'''id of the channel you just created''')
 
