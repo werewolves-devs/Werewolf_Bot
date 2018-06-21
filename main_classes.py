2496ee8f21a2fb60666ecfea21638923e84772b4
Randium <33968886+Randium@users.noreply.github.com>
2018-06-21 17:49:22 +0200

    Added members list to ChannelCreate class
    Now you can add players immediately after having created the channel!

--- main_classes.py
+++ main_classes.py
@@ -6,48 +6,77 @@ class Mailbox:
         self.gamelog = []       # Send message to gamelog channel
         self.botspam = []       # Send message to botspam channel
         self.storytime = []     # Send message to storytime channel
+        self.answer = []        # Send message to triggered channel
         self.channel = []       # Send message to channel
         self.player = []        # Send message to user
         self.newchannels = []   # Create new channel
         self.oldchannels = []   # Edit existing channel
 
-    def log(self,content,temporary = False):
-        """Send a message to the gamelog channel"""
-        self.gamelog.append(Message(content,temporary))
+    def log(self,content,temporary = False,reactions = []):
+        """Send a message to the gamelog channel."""
+        self.gamelog.append(Message(content,temporary,'',reactions))
         return self
     def log_add(self,moar_content):
         """Add some text to the last gamelog message"""
         self.gamelog[-1].add(moar_content)
         return self
+    def log_react(self,emoji):
+        """Add a reaction to the last gamelog message"""
+        self.gamelog[-1].react(emoji)
+        return self
 
-    def spam(self,content,temporary = False):
+    def spam(self,content,temporary = False,reactions = []):
         """Send a message to the botspam channel"""
-        self.botspam.append(Message(content,temporary))
+        self.botspam.append(Message(content,temporary,'',reactions))
         return self
     def spam_add(self,moar_content):
         """Add some text to the last botspam message"""
         self.botspam[-1].add(moar_content)
         return self
+    def spam_react(self,emoji):
+        """Add a reaction to the last botspam message"""
+        self.botspam[-1].react(emoji)
+        return self
 
-    def story(self,content,temporary = False):
+    def story(self,content,temporary = False,reactions = []):
         """Send a message to the storytime channel"""
-        self.storytime.append(Message(content,temporary))
+        self.storytime.append(Message(content,temporary,'',reactions))
         return self
     def story_add(self,moar_content):
         """Add some text to the last storytime message"""
         self.storytime[-1].add(moar_content)
         return self
+    def story_react(self,emoji):
+        """Add a reaction to the last storytime message"""
+        self.storytime[-1].react(emoji)
+        return self
 
-    def msg(self,content,destination,temporary = False):
+    def respond(self,content,temporary = False,reactions = []):
+        """Send a message in the channel where a user has triggered this function."""
+        self.answer.append(Message(content,temporary,'',reactions))
+        return self
+    def respond_add(self,moar_content):
+        """Add some text to the channel that triggered the function."""
+        self.answer[-1].add(moar_content)
+    def respond_react(self,emoji):
+        """Add a reaction to the last message to the channel that triggered the function."""
+        self.answer[-1].react(emoji)
+        return self
+
+    def msg(self,content,destination,temporary = False,reactions = []):
         """Send a message to a given channel"""
-        self.channel.append(Message(content,temporary,destination))
+        self.channel.append(Message(content,temporary,destination,reactions))
         return self
     def msg_add(self,moar_content):
         """Add some text to the last message"""
         self.channel[-1].add(moar_content)
         return self
+    def msg_react(self,emoji):
+        """Add a reaction to the last message"""
+        self.channel[-1].react(emoji)
+        return self
 
-    def dm(self,content,user_id,temporary = False):
+    def dm(self,content,user_id,temporary = False,reactions = []):
         """Send a DM to a given user"""
         self.player.append(Message(content,temporary,user_id))
         return self
@@ -55,10 +84,14 @@ class Mailbox:
         """Add some text to the last DM"""
         self.player[-1].add(moar_content)
         return self
+    def dm_react(self,emoji):
+        """Add a reaction to the last DM"""
+        self.player[-1].react(emoji)
+        return self
   
-    def create_cc(self,channel_name,channel_owner,settlers=[]):
+    def create_cc(self,channel_name,channel_owner,members = [],settlers=[]):
         """Send an order to create a channel"""
-        self.newchannels.append(ChannelCreate(channel_name,channel_owner,settlers))
+        self.newchannels.append(ChannelCreate(channel_name,channel_owner,members,settlers))
         return self
     def edit_cc(self,channel_id,user_id,number):
         """Send an order to edit a channel"""
@@ -67,22 +100,30 @@ class Mailbox:
 
 # Class used to send messages through the mailbox
 class Message:
-    def __init__(self,content,temporary = False,destination = ''):
+    def __init__(self,content,temporary = False,destination = '',reactions=[]):
         self.content = content
         self.temporary = temporary
         self.destination = destination
+        self.reactions = reactions
     def add(self,moar_content):
         self.content += str(moar_content)
+        return self
+    def react(self,emoji):
+        self.reactions.append(emoji)
         return self
 
 # Class for sending commands back to main.py to create/alter channels
 class ChannelCreate:
-    def __init__(self,name,owner,settlers=[]):
+    def __init__(self,name,owner,members=[],settlers=[]):
         self.name = name
         self.owner = owner
+        self.members = []
         self.settlers = []
         
 class ChannelChange:
+    # Notice how settlers is not a value here, while it does happen in games that a user switches standard channels.
+    # This is because settlers is just a database function to execute. When changing a channel, the id is known and
+    # can be executed easily. However, this isn't the case when 
     def __init__(self,channel,victim,number):
         self.channel = channel
         self.victim = victim
