4b26a4b83d38887e82ad81d65ef37a90ce2bf51c
Abdul Rabiu (aka 88) <35082936+abdulrabiu@users.noreply.github.com>
2018-06-25 16:27:42 +0100

    Update functions.py

--- roles_n_rules/functions.py
+++ roles_n_rules/functions.py
@@ -17,3 +17,12 @@ def signup(user_id,emoji,channel):
 
 def syntax_signup():
     pass
+
+def suicide():
+    if player.role.name in ["Spectator", "Dead"] or me.dead == True:
+        return mail = Mailbox().respond("Sorry, but you cannot kill yourself once you are dead!",me.channel)
+    else:
+        me.dead = True
+        player.role.name in ["Dead"]
+        return mail = Mailbox().respond("You have committed suicide, upon closer investigate, it has been found out that " + player.name + " was a " + player.role.name + ".")
+
