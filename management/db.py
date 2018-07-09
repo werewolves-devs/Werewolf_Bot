85e36eec06670d0ae7abad517d926e63cca212aa
Randium <33968886+Randium@users.noreply.github.com>
2018-07-09 21:24:12 +0200

    Prevent double columns during double signup()
    There had been a bug where a user was signed up, then signed off, but was still present in the table for cc's. This way, the column won't be added twice to the table, which otherwise gives multiple errors.

--- management/db.py
+++ management/db.py
@@ -345,10 +345,10 @@ def count_categories():
 
 def get_channel_members(channel_id, number = 1):
     """This function returns a list of user ids that have the given number in a given channel.
-    If it finds none or if the channel wasn't found, the function returns an empty list.  
-    
-    Keyword arguments:  
-    channel_id -> id of the channel  
+    If it finds none or if the channel wasn't found, the function returns an empty list.
+
+    Keyword arguments:
+    channel_id -> id of the channel
     number -> number that is to be selected"""
     c.execute("SELECT * FROM 'channel_rows'")
     members = []
@@ -364,22 +364,24 @@ def get_channel_members(channel_id, number = 1):
 # Add a new participant to the database
 def signup(user_id,name,emoji):
     c.execute("INSERT INTO 'game'('id','name','emoji') VALUES (?,?,?);", (user_id,name,emoji))
-    c.execute("ALTER TABLE channels ADD COLUMN 'id{}' TEXT NOT NULL DEFAULT 0".format(user_id))
-    c.execute("INSERT INTO channel_rows ('id') VALUES (?)",(user_id,))
+    c.execute("SELECT * FROM channel_rows WHERE id =?",(user_id,))
+    if c.fetchall() == None:
+        c.execute("ALTER TABLE channels ADD COLUMN 'id{}' TEXT NOT NULL DEFAULT 0".format(user_id))
+        c.execute("INSERT INTO channel_rows ('id') VALUES (?)",(user_id,))
     conn.commit()
 
 def add_poll(msg_table,purpose,user_id = 0):
-    """Add a new poll to the database. The poll is saved so it can be evaluated later on.  
-    
-    msg_table -> the list of messages that contain the poll  
-    purpose -> the reason the poll is made for. Examples are 'wolf', 'mayor', 'lynch' or 'cult'  
+    """Add a new poll to the database. The poll is saved so it can be evaluated later on.
+
+    msg_table -> the list of messages that contain the poll
+    purpose -> the reason the poll is made for. Examples are 'wolf', 'mayor', 'lynch' or 'cult'
     user_id -> the user who is \"responsible\" for the lynch. Leave blank if daily poll."""
     amount = int(max_participants/20)+1
     if msg_table == []:
         raise ValueError("Cannot insert empty poll into database.")
     if len(msg_table) > amount:
         raise IndexError("Poll needed more space in database than expected!")
-    
+
     request_msg = "INSERT INTO 'polls' ('purpose','user_id'"
     request2_msg = ") VALUES ('{}',{}".format(purpose,user_id)
 
@@ -387,7 +389,7 @@ def add_poll(msg_table,purpose,user_id = 0):
 
         request_msg += ",'part{}'".format(i+1)
         request2_msg += ",{}".format(msg_table[i].id)
-    
+
     query = request_msg + request2_msg + ");"
     c.execute(query)
     conn.commit()
@@ -401,13 +403,13 @@ def get_all_polls():
     conn.commit()
 
     return [PollToEvaluate(item) for item in answer_table]
-    
+
 def add_freezer(user_id,victim_id,role):
     """This function saves an ice king's guess in the database. If a guess about that player already exists,
-    it is overwritten, and the old role is returned.  
-    
-    user_id -> the role of the ice king casting the guess  
-    victim_id -> the player whose role is guessed  
+    it is overwritten, and the old role is returned.
+
+    user_id -> the role of the ice king casting the guess
+    victim_id -> the player whose role is guessed
     role -> the role that victim_id is guessed to be  """
     c.execute("SELECT role FROM 'freezers' WHERE victim =?",(victim_id,))
     answer = c.fetchone()
@@ -420,17 +422,17 @@ def add_freezer(user_id,victim_id,role):
     return answer[0]
 
 def get_freezers(user_id):
-    """Get a list of all the guesses an ice king has made so far, including their roles.  
-    
+    """Get a list of all the guesses an ice king has made so far, including their roles.
+
     user_id -> the id of the ice king"""
     c.execute("SELECT victim, role FROM freezers WHERE king =?",(user_id,))
     return c.fetchall()
 
 def delete_freezer(user_id,victim_id):
     """Remove a guessed user from the database. If the user wasn\'t in the database, return False.
-    Else, return True when removed successfully.  
-    
-    user_id -> the player removing the guess  
+    Else, return True when removed successfully.
+
+    user_id -> the player removing the guess
     victim_id -> the player being guessed"""
     c.execute("SELECT * FROM freezers WHERE king =? AND victim =?",(user_id,victim_id))
     if c.fetchone() == None:
