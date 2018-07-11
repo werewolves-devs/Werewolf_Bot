e40a333a01ce1d3c254ed3060493ce4617ef5161
Randium <33968886+Randium@users.noreply.github.com>
2018-07-11 12:22:25 +0200

    Fixing a bug; removing ONE underscore
    Yes.

--- story_time/cc_creation.py
+++ story_time/cc_creation.py
@@ -7,7 +7,7 @@ def cc_intro(list):
 
     if randium.random() < 0.01:
         msg = '''Once upon a time there was a duck. The duck pooped out an entire conspiracy channel.'''
-        msg_table.append(msg)
+        msgtable.append(msg)
     
     if amount == 0:
         return "That is strange... a channel with zero members?"
