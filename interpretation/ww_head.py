558f865aa8c6c00fafa7e92a5c762c8f3dd860fd
ryandezinator <41645810+ryandezinator@users.noreply.github.com>
2018-08-16 11:30:06 +0200

    Update ww_head.py

--- interpretation/ww_head.py
+++ interpretation/ww_head.py
@@ -871,8 +871,8 @@ def process(message, isGameMaster=False, isAdmin=False):
         # TODO
         return todo()
     if is_command(message, ['profile'], True):
-        # TODO
-        return todo()
+        msg = "**USAGE:** The use of this command is to check your own profile, you can check other peoples profiles by adding their name. /n/n`" + prefix + "profile<persons name>\`n\n**Example:** `!profile or !profile Randium`"
+        return [Mailbox().respond(msg,True)]
     help_msg += "`" + prefix + "profile` - See a player's profile.\n"
 
     '''signup'''
