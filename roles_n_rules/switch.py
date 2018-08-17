04787aba652462104e3e140c6ddab4f3123475fc
Bram <bramspammail003@gmail.com>
2018-08-18 00:51:23 +0200

    Finish part 1 of starting the day (hopefully)
    I'm not sure if this is it, but I think it is.

--- roles_n_rules/switch.py
+++ roles_n_rules/switch.py
@@ -68,6 +68,11 @@ def pay():
 
         # Remove tanner disguises
         db_set(user_id,'fakerole',user_role)
+        
+        # Remove zombie tag
+        db_set(user_id,'bitten',0)
+
+        return answer
 
 def start_game():
     """This function is triggered at the start of the game. If successful, the function returns a Mailbox.
