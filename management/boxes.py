4f8f83453a3138e0815080f33c68bd1a73ea57ac
Bram <bramspammail003@gmail.com>
2018-08-28 21:58:42 +0200

    Add final commands for interaction with token database

--- management/boxes.py
+++ management/boxes.py
@@ -40,7 +40,7 @@ def add_options(token,choice1,choice2,choice3):
     """Register the three options into the database.  
 
     Keyword arguments:  
-    token -> the choices' token  
+    token -> the options' token  
     choice1 -> option 1  
     choice2 -> option 2  
     choice3 -> option 3"""
@@ -48,3 +48,20 @@ def add_options(token,choice1,choice2,choice3):
     c.execute("UPDATE 'tokens' SET 'opt2' =? WHERE 'token' =?",(choice2,token))
     c.execute("UPDATE 'tokens' SET 'opt3' =? WHERE 'token' =?",(choice3,token))
     conn.commit()
+
+def add_choice(token,choice):
+    """Register the chosen option into the database.  
+    
+    Keyword arguments:  
+    token -> the choice's token  
+    choice -> the made choice"""
+    c.execute("UPDATE 'tokens' SET 'choice' =? WHERE 'token' =?",(choice,token))
+    conn.commit()
+
+def get_token_data(token):
+    """Gain the info about a specific token  
+    
+    Keyword arguments:  
+    token -> the token"""
+    c.execute("SELECT * FROM 'tokens' WHERE 'token' =?",(token,))
+    return c.fetchone()
