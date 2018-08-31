f8efbf5cabe1b534269c867b52579ab69f38077a
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-08-31 19:38:28 +0100

    add create_box function

--- communication/box.py
+++ communication/box.py
@@ -1,20 +1,29 @@
 import management.boxes as box
+import config
+from requests import get
 
 def request_token(user_id):
-    """Request a lootbox token from the API.  
-    The function returns a string, which is the URL to the lootbox in a string format.  
-    
-    Keyword arguments:  
+    """Request a lootbox token from the API.
+    The function returns a string, which is the URL to the lootbox in a string format.
+
+    Keyword arguments:
     user_id -> user for whom the lootbox is meant."""
 
-    # To be filled in by BenTechy66
-    return 'https://discordapp.com/login'
+    url = config.base_url + "/api/create_lootbox.php?auth_token=" + config.box_token + "&discord_id=" + user_id
+
+    response = get(url).text
+    if '200' not in response:
+        # There's an Error
+        print("Nah fam ya gotta check your error: " + response)
+        return
+
+    return response.split(":")[1]
 
 def delete_token(token):
-    """Request the API to invalidate a given token.  
-    This function can be used for timeouts, invalid requests, specific retractions or cleaning up the database.  
-    
-    Keyword arguments:  
+    """Request the API to invalidate a given token.
+    This function can be used for timeouts, invalid requests, specific retractions or cleaning up the database.
+
+    Keyword arguments:
     token -> the token that needs to be made invalid."""
 
     # To be filled in by BenTechy66
