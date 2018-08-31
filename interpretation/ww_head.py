cda024b5c4a28fb9596c1058be724b0f0685217a
Theret <fabi3nr0billard@gmail.com>
2018-08-31 07:59:11 +0100

    Allow Dog to use dog ability

--- interpretation/ww_head.py
+++ interpretation/ww_head.py
@@ -624,13 +624,10 @@ def process(message, isGameMaster=False, isAdmin=False, isPeasant=False):
             '''follow'''
             # The command that allows the dog to choose a side.
             if is_command(message, ['bark', 'become', 'choose', 'follow']) and user_role == "Dog":
-                target = check.users(message,1,True,True)
-                if not target:
-                    return [Mailbox().respond("**INVALID SYNTAX:**\nPlease make sure to mention a user.\n\n**Tip:** You can also mention their emoji!",True)]
                 disguise = check.roles(message,1)
                 if not disguise:
                     return [Mailbox().respond("**INVALID SYNTAX:** \nPlease make sure to provide a role.")]
-                return [func.disguise(user_id,target[0],disguise[0])]
+                return [func.dog_follow(user_id,disguise[0])]
             if is_command(message,['bark','become','choose','follow'],True) and user_role == "Dog":
                 msg = "**Usage:** Choose a role to play as.\n\n`" + prefix + "choose <role>`\n\n"
                 msg += "**Example:** `" + prefix + "choose Innocent`\nThe options are **Innocent**, **Cursed Civilian** and **Werewolf**. "
