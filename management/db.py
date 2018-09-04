d5f317d251f11473c54859403479020611fbdab7
Bram <bramspammail003@gmail.com>
2018-09-04 17:54:12 +0200

    The bot will FINALLY acknowledge someone died!
    That took a while to discover! And suddenly, WHAM! I found it!

--- management/db.py
+++ management/db.py
@@ -400,8 +400,8 @@ def has_amulet(user_id):
 
 def insert_deadie(user_id):
     """Add a new deadie to the list. This list will be evaluated for the storytime."""
-    c.execute("SELECT * FROM 'deadies' WHERE 'user_id' =?",(user_id,))
-    if c.fetchall == []:
+    c.execute("SELECT * FROM 'deadies' WHERE user_id =?",(user_id,))
+    if c.fetchall() == []:
         c.execute("INSERT INTO 'deadies' ('user_id') VALUES (?);",(user_id,))
     conn.commit()
 
