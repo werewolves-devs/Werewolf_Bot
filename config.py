202f48244ed6ae4f8da3b2f2ffe508e4bfbe6b6a
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-09-08 21:01:39 +0100

    rework config

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ WW_TOKEN = "NDAxNzE2MzgwODAzNTMwNzUy.Dm2kxA.r_PwUl5gZE0P9EFDZHu-7XKyyss"
 TM_TOKEN = "yo_poppas_token"
 DV_TOKEN = "yo_devils_token"
 GH_TOKEN = "yo_deadies_token"
-discord_webhook = "https://discordapp.com/api/webhooks/488075058536490522/a0bNalwX7yeci-mBAHaiSjkOmnRfxErBBmrfvSTv6_C2Ey89j00jhkcKWfBbp-Uf8l_Y"
+WEBHOOK_ID = "488075058536490522"
+WEBHOOK_TOKEN = "a0bNalwX7yeci-mBAHaiSjkOmnRfxErBBmrfvSTv6_C2Ey89j00jhkcKWfBbp-Uf8l_Y"
 
 # Rules 'n' settings
 max_channels_per_category = 50
