782f5a312cc228116178abef5de2be09b814c92a
Unknown <bramspammail003@gmail.com>
2018-09-09 06:14:17 +0200

    Finish improvement of webhook module

--- communication/webhook.py
+++ communication/webhook.py
@@ -1,10 +1,16 @@
 import requests
 import discord
 from discord import Webhook, RequestsWebhookAdapter
-from config import WEBHOOK_ID, WEBHOOK_TOKEN
+from config import WEBHOOK_PUBLIC_ID, WEBHOOK_PUBLIC_TOKEN, WEBHOOK_PRIVATE_ID, WEBHOOK_PRIVATE_TOKEN
 
-def send_message(content):
-    webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN,\
+def send_public_message(content):
+    webhook = Webhook.partial(WEBHOOK_PUBLIC_ID, WEBHOOK_PUBLIC_TOKEN,\
+     adapter=RequestsWebhookAdapter())
+
+    webhook.send(content)
+
+def send_private_message(content):
+    webhook = Webhook.partial(WEBHOOK_PRIVATE_ID, WEBHOOK_PRIVATE_TOKEN,\
      adapter=RequestsWebhookAdapter())
 
     webhook.send(content)
