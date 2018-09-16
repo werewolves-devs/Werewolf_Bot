290cdc87465b3114592a7f94a139daf0de076146
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-09-16 15:41:33 +0100

    Add auth in app.py and threading

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from communication.website.website import app
+from config import oauth_secret
+app.config['SECRET_KEY'] = oauth_secret
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='80')
+    app.run(host='0.0.0.0',port=5000,threaded=True)
