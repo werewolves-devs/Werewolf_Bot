a78be3073523a720c9b2df9827a6afa1347905a6
Unknown <bramspammail003@gmail.com>
2018-10-13 03:36:16 +0200

    Finish shop mechanics

--- hardcore_reset.py
+++ hardcore_reset.py
@@ -55,7 +55,7 @@ def hard_reset(skip = False):
     c.execute("CREATE TABLE 'offers' ('id' INTEGER NOT NULL, 'emoji' TEXT NOT NULL, 'price' INTEGER NOT NULL, 'owner' INTEGER NOT NULL, FOREIGN KEY(`id`) REFERENCES `users`(`id`));")
     c.execute("CREATE TABLE 'requests' ('id' INTEGER NOT NULL, 'emoji' TEXT NOT NULL, 'price' INTEGER NOT NULL, 'owner' INTEGER NOT NULL, FOREIGN KEY(`id`) REFERENCES `users`(`id`));")
     c.execute("CREATE TABLE 'tokens' ( `token` TEXT NOT NULL, `owner` INTEGER NOT NULL, `status` INTEGER NOT NULL DEFAULT 0, `opt1` NUMERIC, `opt2` TEXT, `opt3` TEXT, `choice` TEXT, `source1` TEXT, `source2` TEXT, `creation` TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, `redeemed` TEXT, `message` INTEGER NOT NULL, FOREIGN KEY(`owner`) REFERENCES `users`(`id`), PRIMARY KEY(`token`,`message`) )")
-    c.execute("CREATE TABLE 'shops' ('item' INTEGER NOT NULL, 'price' REAL NOT NULL, 'bought' INTEGER NOT NULL DEFAULT 0, 'oldprice' REAL NOT NULL DEFAULT 0, PRIMARY KEY('message'));")
+    c.execute("CREATE TABLE 'shops' ('item' INTEGER NOT NULL, 'price' REAL NOT NULL, 'bought' INTEGER NOT NULL DEFAULT 0, 'oldprice' REAL NOT NULL DEFAULT 0, PRIMARY KEY('item'));")
     c.execute("CREATE TABLE 'sources' ('user' INTEGER NOT NULL, 'source' TEXT NOT NULL, 'amount' INTEGER NOT NULL DEFAULT 1, FOREIGN KEY(`user`) REFERENCES `users`(`id`));")
     print('Formatting completed! The bot is now ready for a new game!\n')
 
--- management/shop.py
+++ management/shop.py
@@ -7,19 +7,20 @@ c = conn.cursor()
 
 def __create_shop():
     c.execute("SELECT item, price FROM 'shops'")
-    return [(int(item[0]), int(item[1])) for item in c.fetchall()]
+    return [(int(item[0]), item[1]+0.5) for item in c.fetchall()]
 
 def __item_traded_for_price(item,price):
-    c.execute("UPDATE 'shop' SET bought = bought + 1 WHERE item=?",(item,))
-    c.execute("UPDATE 'shop' SET price = (price + ?)/2 WHERE item=? AND bought > 1",(price,item))
+    c.execute("UPDATE 'shops' SET bought = bought + 1 WHERE item=?",(item,))
+    c.execute("UPDATE 'shops' SET price = (price + ?)/2 WHERE item=? AND bought > 1",(price,item))
     conn.commit()
 
 def __refresh():
-    c.execute("SELECT item, price, oldprice FROM 'shop' WHERE bought > 1;")
+    c.execute("SELECT item, price, oldprice FROM 'shops' WHERE bought > 1;")
     changed_items = c.fetchall()
 
-    c.execute("UPDATE 'shop' SET bought = 0")
-    c.execute("UPDATE 'shop' SET oldprice = price")
+    c.execute("UPDATE 'shops' SET bought = 0")
+    c.execute("UPDATE 'shops' SET oldprice = price")
+    conn.commit()
     return [(int(item[0]), int(item[1]), int(item[2])) for item in changed_items if int(item[1]) != int(item[2])]
 
 def get_market_message():
@@ -28,11 +29,11 @@ def get_market_message():
 
     shop = __create_shop()
 
-    for i in range(len(shop.items)):
-        item = shop.items[i]
+    for i in range(len(shop)):
+        item = shop[i]
         item_name = items.int_to_item(item[0])
-        price_to_buy = int(1.1 * item[1])
-        price_to_sell = int(0.9 * item[2])
+        price_to_buy = max(int(1.1 * item[1]),int(item[1])+1)
+        price_to_sell = min(int(0.9 * item[1]),max(1,int(item[1]-1)))
 
         item_text = "**[{}]** {}\n**Buy:** {} credits.\n**Sell:** {} credits.\n\n".format(i+1,item_name,price_to_buy,price_to_sell)
         
@@ -46,12 +47,15 @@ def get_market_message():
     return msg_table
 
 def buy(user_id,number):
-    i = number - 1
+    i = int(number) - 1
     shop = __create_shop()
 
-    item = shop.items[i]
+    if i >= len(shop) or i < 0:
+        return "Invalid value! Please choose a provided number!"
+
+    item = shop[i]
     item_name = items.int_to_item(item[0])
-    price_to_buy = int(1.1 * item[1])
+    price_to_buy = max(int(1.1 * item[1]),int(item[1])+1)
 
     if inventory.has_item(user_id,0,False) < price_to_buy:
         return "I am sorry! You do not have enough credits to buy one {}!".format(item_name)
@@ -64,12 +68,15 @@ def buy(user_id,number):
     return "You have successfuly bought one {}!\nType `".format(item_name) + config.devil_prefix + "inventory` to view your inveentory."
 
 def sell(user_id,number):
-    i = number - 1
+    i = int(number) - 1
     shop = __create_shop()
 
-    item = shop.items[i]
+    if i >= len(shop) or i < 0:
+        return "Invalid value! Please choose a provided number!"
+
+    item = shop[i]
     item_name = items.int_to_item(item[0])
-    price_to_sell = int(0.9 * item[1])
+    price_to_sell = min(int(0.9 * item[1]),max(1,int(item[1]-1)))
 
     if not inventory.has_item(user_id,item[0]):
         return "I am sorry! You cannot sell one {} if you do not have one!".format(item_name)
@@ -79,7 +86,8 @@ def sell(user_id,number):
 
     __item_traded_for_price(item[0],price_to_sell)
 
-    return "You have successfuly bought one {}!\nType `".format(item_name) + config.devil_prefix + "inventory` to view your inventory."
+    return "You have successfuly sold one {}!\nType `".format(item_name) + config.devil_prefix + "inventory` to view your inventory."
 
 def refresh_market():
-    pass
+    return __refresh()
+
