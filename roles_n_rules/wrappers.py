9f2538f5791d35194870d93f56c16ac2434af783
Unknown <bramspammail003@gmail.com>
2018-10-15 09:13:37 +0200

    Add kill exception wrappers

---
+++ roles_n_rules/wrappers.py
@@ -0,0 +1,60 @@
+from functools import wraps
+
+next = '|            '
+success = '|---> '
+failure = '|'
+skull = '💀 '
+
+def demonized(func):
+    @wraps(func)
+    def validate_if_demonized(*args,**kwargs):
+        if args[0].demonized == True:
+            # TODO: Make the user undead.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_demonized
+
+def sleepingover(func):
+    @wraps(func)
+    def validate_if_sleepingover(*args,**kwargs):
+        if args[0].sleepingover == True:
+            # TODO: Cancel the attack.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_sleepingover
+
+def frozen(func):
+    @wraps(func)
+    def validate_if_frozen(*args,**kwargs):
+        if args[0].frozen == True:
+            # TODO: Cancel the attack.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_frozen
+
+def souls(func):
+    @wraps(func)
+    def validate_if_souls(*args,**kwargs):
+        if args[0].souls > 0:
+            # TODO: Make the user undead.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_souls
+
+def protected(func):
+    @wraps(func)
+    def validate_if_protected(*args,**kwargs):
+        if args[0].protected == True:
+            # TODO: Cancel the attack.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_protected
+
+def abducted(func):
+    @wraps(func)
+    def validate_if_abducted(*args,**kwargs):
+        if args[0].abducted == True:
+            # TODO: Cancel the attack.
+            pass
+        return func(*args,**kwargs)
+    return validate_if_abducted
