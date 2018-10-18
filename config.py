515d2121038c2d0f0b8617d572edcdac82443180
Randium <randium003@gmail.com>
2018-10-18 02:49:22 +0200

    Add Enums to config file

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
+from enum import Enum
+
 # Discord API / Login settings
-WW_TOKEN = "NDAxNzE2MzgwODAzNTMwNzUy.Dm2kxA.r_PwUl5gZE0P9EFDZHu-7XKyyss"
+WW_TOKEN = "yo_mommas_token"
 TM_TOKEN = "yo_poppas_token"
 DV_TOKEN = "yo_devils_token"
 GH_TOKEN = "yo_deadies_token"
 
-WEBHOOK_PUBLIC_ID = "488220391391887362"
-WEBHOOK_PUBLIC_TOKEN = "2fp9iH072W48g0QbSo0us0zo5v0qQvKGzo618aOq_uryjUrV1r7Bn_pIhQL0Nu9E-nNC"
-WEBHOOK_PRIVATE_ID = "488075058334400522"
-WEBHOOK_PRIVATE_TOKEN = "a0bNalwX7yeci-mBAk50WjCtl2OfxErBBmrfvSTv6_C2Ey89j00jhkcKWfBbp-Uf8l_Y"
+WEBHOOK_PUBLIC_ID = "mah_fancy_id"
+WEBHOOK_PUBLIC_TOKEN = "mah_fancy_token"
+WEBHOOK_PRIVATE_ID = "yah_fancy_id"
+WEBHOOK_PRIVATE_TOKEN = "yah_fancy_token"
 
 # Rules 'n' settings
 max_channels_per_category = 50
 max_participants = 40
 max_cc_per_user = 8
 activity_hours = 72
-season = "1"
+season = "0"
 
 ww_prefix = '!'
 ghost_prefix = '$'
@@ -31,21 +33,23 @@ item_file = 'storage/items.json'
 max_channels_per_category = 50
 
 # List of specific channels
-welcome_channel = 425036088239980544
-game_log = 485843514379337748
-bot_spam = 394541968689987584
-story_time = 485843933625188352
-shop_channel = 394541968689987584
-quotes = 487354468606803968
+class Destination(Enum):
+    welcome_channel = 425036088239980544
+    game_log = 485843514379337748
+    bot_spam = 394541968689987584
+    story_time = 485843933625188352
+    market = 394541968689987584
+    quotes = 487354468606803968
 
 # List of specific roles
-administrator = 400956179561447425
-game_master = 375651524950360075
-participant = 380494164627816448
-dead_participant = 380493972100874242
-frozen_participant = 432146998762668033
-suspended = 382947666935414784
-peasant = 396041262836351006
+class Role(Enum):
+    administrator = 400956179561447425
+    game_master = 375651524950360075
+    participant = 380494164627816448
+    dead_participant = 380493972100874242
+    frozen_participant = 432146998762668033
+    suspended = 382947666935414784
+    peasant = 396041262836351006
 
-mayor = 382103216105717760
-reporter = 382103338550034442
+    mayor = 382103216105717760
+    reporter = 382103338550034442
