57c9003b627b83fbb58bc00c206e8d5ce61c9f66
Randium <randium003@gmail.com>
2018-10-18 02:50:07 +0200

    Add requirement wrappers for role-specific commands

--- roles_n_rules/wrappers.py
+++ roles_n_rules/wrappers.py
@@ -66,3 +66,43 @@ def require_uses(func):
             return func(*args,**kwargs)
         pass # TODO: Cancel the power
     return more_than_zero_uses
+
+def require_numbers(amount):
+    def search_numbers(func):
+        @wraps(func)
+        def look_for_numbers(self,numbers,roles,users):
+            if len(numbers) < amount:
+                pass # TODO: Cancel powers due to no integers provided.
+            return func(self,numbers,roles,users)
+        return look_for_numbers
+    return search_numbers
+
+def require_roles(amount):
+    def search_roles(func):
+        @wraps(func)
+        def look_for_roles(self,numbers,roles,users):
+            if len(roles) < amount:
+                pass # TODO: Cancel powers due to no integers provided.
+            return func(self,numbers,roles,users)
+        return look_for_roles
+    return search_roles
+
+def require_users(amount):
+    def search_users(func):
+        @wraps(func)
+        def look_for_users(self,numbers,roles,users):
+            if len(users) < amount:
+                pass # TODO: Cancel powers due to no roles provided
+            return func(self,numbers,roles,users)
+        return look_for_users
+    return search_users
+
+def power_abducted(func):
+    @wraps(func)
+    def remove_abducted_players(self,numbers,roles,users):
+        for other in users:
+            if other.abducted:
+                pass # TODO: Cancel power due to targeting an abducted player.
+        
+        return func(self,numbers,roles,users)
+    return remove_abducted_players
